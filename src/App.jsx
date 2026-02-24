cf1ec316cebd113afb2214087351f933df9f1d7b
Sadhgun-Reddy <sadhgunreddy09@gmail.com>
2026-02-25 00:33:27 +0530

    carter_dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
 import DashboardLayout from './layouts/DashboardLayout';
 import Dashboard from './pages/Dashboard';
+import CatererLayout from './layouts/CatererLayout';
+import CatererDashboard from './pages/CatererDashboard';
 
 function App() {
     return (
@@ -11,6 +13,9 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/dashboard" element={<DashboardLayout />}>
                     <Route index element={<Dashboard />} />
+                </Route>
+                <Route path="/caterer" element={<CatererLayout />}>
+                    <Route index element={<CatererDashboard />} />
                 </Route>
             </Routes>
         </Router>
